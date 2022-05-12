80fe6f88053abbf1dd74615e477565aa70081e08
mujeex <mujahid.bappai@yahoo.com>
2022-05-12 12:20:49 +0100

    chore: install truffle hd wallet package

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,30 @@
 const path = require("path");
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = 'grit price orient bamboo cement add mad elbow february planet myth morning'
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   // contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
+    development: {
+      // Removed to get Truffle Test to pass for Oracles 
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+      },
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: '*',
+      gas: 6721975 // 9999999
+    }
+  },
     develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
     }
-  },
+,
   compilers: {
     solc: {
       version: "^0.8.13"
